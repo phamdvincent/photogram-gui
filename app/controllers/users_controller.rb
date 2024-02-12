0d954a4d244406a11e575a9c7921a8d1cd7456d7
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-12 17:36:29 +0000

    Added update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    username = params.fetch("username")
-    user_id = User.where({ :username => username}).at(0).id
+    path_username = params.fetch("path_username")
+    user_id = User.where({ :username => path_username}).at(0).id
     @user = User.where({ :id => user_id}).at(0)
 
     @own_photos = @user.own_photos
@@ -22,6 +22,20 @@ class UsersController < ApplicationController
       redirect_to("/users", { :notice => "User created successfully." })
     else
       redirect_to("/users", { :notice => "User failed to be created." })
+    end
+  end
+
+  def update
+    path_username = params.fetch("path_username")
+    user_id = User.where({ :username => path_username}).at(0).id
+    @update_user = User.where({ :id => user_id }).at(0)
+    @update_user.username = params.fetch("input_username")
+
+    if @update_user.valid?
+      @update_user.save
+      redirect_to("/users/#{@update_user.username}", { :notice => "User updated successfully." })
+    else
+      redirect_to("/users/#{@update_user.username}", { :notice => "User failed to updated." })
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get("/", { :controller => "users", :action => "index"})
 
   get("/users", { :controller => "users", :action => "index"})
-  get("users/:username", { :controller => "users", :action => "show"})
+  get("/users/:path_username", { :controller => "users", :action => "show"})
   post("/add_user", { :controller => "users", :action => "create" })
+  post("/update_user/:path_username", { :controller => "users", :action => "update" })
 
 
   get("/photos", { :controller => "photos", :action => "index" })
